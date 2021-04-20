d3e9e4332193a7bbb6e29c6facd6b0fd5db0c36a
Leo Barnuevo <barnuevoleonardo@gmail.com>
2021-04-20 12:40:24 -0500

    second commit

--- js/site.js
+++ js/site.js
@@ -1,9 +1,9 @@
 //when the user clicks the button
 function buildPaymentSchedule() {
     //get some values
-    let paymentArray = [];
-    paymentArray = getPayments();
-    displayData(paymentArray);
+
+    let payments = getPayments();
+    displayData(payments);
 
 }
 
@@ -11,6 +11,7 @@ function buildPaymentSchedule() {
 
 //build you array of payments
 function getPayments() {
+    let paymentArray = [];
 
     var loanAmount = parseInt(document.getElementById("loanAmount").value);
     var paymentTerm = parseInt(document.getElementById("paymentTerm").value);
@@ -20,7 +21,6 @@ function getPayments() {
     var remainingBalance = loanAmount;
     var totalInterest = 0;
 
-    let paymentArray = [];
 
 
 
@@ -33,8 +33,10 @@ function getPayments() {
         let principalPayment = totalMonthlyPayments - interestPayment;
         remainingBalance = remainingBalance - principalPayment;
         totalInterest += interestPayment
+        var month = index + 1;
 
-        obj["index"] = index;
+
+        obj["month"] = month;
         obj["totalMonthlyPayments"] = totalMonthlyPayments;
         obj["principalPayment"] = principalPayment;
         obj["interestPayment"] = interestPayment;
@@ -60,13 +62,26 @@ function displayData(paymentArray) {
     for (let i = 0; i < paymentArray.length - 1; i++) {
         const dataRow = template.content.cloneNode(true);
 
-        dataRow.getElementById("monthOut").textContent = paymentArray[i].index;
-        dataRow.getElementById("paymentOut").textContent = paymentArray[i].totalMonthlyPayments;
-        dataRow.getElementById("principalOut").textContent = paymentArray[i].principalPayment;
-        dataRow.getElementById("interestOut").textContent = paymentArray[i].interestPayment;
-        dataRow.getElementById("totalInterestOut").textContent = (paymentArray[i].totalInterest);
-        dataRow.getElementById("balanceOut").textContent = (paymentArray[i].balance);
+        dataRow.getElementById("monthOut").textContent = paymentArray[i].month;
+        dataRow.getElementById("paymentOut").textContent = paymentArray[i].totalMonthlyPayments.toFixed(2);
+        dataRow.getElementById("principalOut").textContent = paymentArray[i].principalPayment.toFixed(2);
+        dataRow.getElementById("interestOut").textContent = paymentArray[i].interestPayment.toFixed(2);
+        dataRow.getElementById("totalInterestOut").textContent = (paymentArray[i].totalInterest).toFixed(2);
+        dataRow.getElementById("balanceOut").textContent = Math.abs((paymentArray[i].balance).toFixed(2));
 
         resultsBody.appendChild(dataRow);
     }
+
+    document.getElementById("monthlyPayments").innerText = paymentArray[0].totalMonthlyPayments.toFixed(2);
+    let principal = document.getElementById("loanAmount").value;
+    document.getElementById("totalPrincipal").innerText = principal
+    let ttlInterest = paymentArray[paymentArray.length - 1].totalInterest;
+    document.getElementById("totalInterest").innerText = ttlInterest.toFixed(2);
+    document.getElementById("totalCost").innerText = (parseFloat(ttlInterest) + parseInt(principal)).toFixed(2);
+
+
 }
+
+$(document).ready(function () {
+    $('#myTable').DataTable();
+});
